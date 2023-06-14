89f24b486ff77881c6ef8ab9a3a3a25ebbc65a6b
Tudor Berechet <77977126+dulap16@users.noreply.github.com>
2023-06-14 23:33:57 +0300

    Try to draw a circle

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 
 const canvas = document.getElementById("canvas");
 const pen = canvas.getContext("2d");
+let canvasWidth = canvas.scrollWidth;
+let canvasHeight = canvas.scrollHeight;
+
+let centerx = canvasWidth / 2;
+let centery = (canvasHeight / 10) * 9;
+let initRadius = 100;
+
+pen.fillStyle = "#000000";
+pen.beginPath();
+pen.arc(centerx, centery, initRadius, 0, Math.PI);
+pen.stroke();
+
