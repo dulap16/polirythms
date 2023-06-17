d51a732e26fdbc5330ff62d07c873360b70dfd06
dulap16 <tudorbrk8@yahoo.com>
2023-06-17 15:08:30 +0300

    Revert "Add drawBaseLine() method and call it"
    This reverts commit 21411d3526508095b5d32ad47e44b7cdfa4f0952.

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
 const canvas = document.getElementById("canvas");
 const pen = canvas.getContext("2d");
 
+const draw = () => {
 
-const drawBaseLine = () => {
-    canvas.width = canvas.clientWidth;
-    const canvasPos = {
-        x: canvas.clientLeft,
-        y: canvas.clientTop
-    };
-    console.log(canvas.clientWidth + ' ' + canvas.clientHeight)
-
-
-    const startPoint = {
-        x: canvas.width * 0.1,
-        y: canvas.height * 0.9
-    }
-
-    const endPoint = {
-        x: canvas.width * 0.9,
-        y: canvas.height * 0.9
-    }
-
-    console.log(startPoint.x + ' ' + startPoint.y + '\n' + endPoint.x + ' ' + endPoint.y);
-
-    pen.strokeStyle = "#D0D0D0";
-    pen.lineWidth = 5;
-
-
-    pen.beginPath();
-    pen.moveTo(startPoint.x, startPoint.y);
-    pen.lineTo(endPoint.x, endPoint.y);
-    pen.stroke();
 }
-
-drawBaseLine();
