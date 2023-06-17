778e1e3f02b83fcf46c2e75ea28eb812386456f1
dulap16 <tudorbrk8@yahoo.com>
2023-06-17 15:28:54 +0300

    Make drawBaseLine() take start and end as parameters

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
 const canvas = document.getElementById("canvas");
 const pen = canvas.getContext("2d");
 
-const drawBaseLine = () => {
+const drawBaseLine = (startPoint, endPoint) => {
 
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
 
-    const canvasPos = {
-        x: canvas.clientLeft,
-        y: canvas.clientTop
-    };
-    console.log(canvas.clientWidth + ' ' + canvas.clientHeight)
-
-
-    const startPoint = {
-        x: canvas.width * 0.1,
-        y: canvas.height * 0.9
-    }
-
-    const endPoint = {
-        x: canvas.width * 0.9,
-        y: canvas.height * 0.9
-    }
-
-    console.log(startPoint.x + ' ' + startPoint.y + '\n' + endPoint.x + ' ' + endPoint.y);
-
     pen.strokeStyle = "#D0D0D0";
     pen.lineWidth = 5;
-
 
     pen.beginPath();
     pen.moveTo(startPoint.x, startPoint.y);
@@ -35,4 +15,13 @@ const drawBaseLine = () => {
     pen.stroke();
 }
 
-drawBaseLine();
+    x: canvas.width * 0.1,
+    y: canvas.height * 0.9
+}
+
+const endPoint = {
+    x: canvas.width * 0.9,
+    y: canvas.height * 0.9
+}
+
+drawBaseLine(startPoint, endPoint);
