9613cc61d785cc41c9e8e76be165521d75c51c03
dulap16 <tudorbrk8@yahoo.com>
2023-06-23 22:06:55 +0300

    Change toggleSound() to toggleSoundClicked()
    This method now changes the color of the toggle object in the scene

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("canvas");
 const slider = document.getElementById("slider");
+const soundToggle = document.getElementById("sound-toggle");
 const pen = canvas.getContext("2d");
 const nrOfArcs = 15;
 
@@ -27,8 +28,14 @@ document.onvisibilitychange = () => soundOn = false;
 let nextHits = [];
 let sounds = [];
 
-const toggleSound = () => {
-    soundOn = !soundOn;
+function toggleSoundClicked() {
+    if (soundOn) {
+        soundOn = false;
+        soundToggle.style.backgroundColor = "red";
+    } else {
+        soundOn = true;
+        soundToggle.style.backgroundColor = "green";
+    }
 }
 document.onclick = () => toggleSound();
 
